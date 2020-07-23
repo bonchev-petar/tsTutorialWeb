18fc8b357d4e3f08ca093de5863927766b36cf33
Petar Bonchev <bonchevpetar@gmail.com>
2020-07-23 22:56:05 +0300

    first commit

---
+++ src/models/User.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosResponse } from "axios";
+import { Eventing } from "./Eventing";
+
+interface UserProps {
+  id?: number;
+  name?: string;
+  age?: number;
+}
+
+export class User {
+  public events: Eventing = new Eventing();
+  constructor(private data: UserProps) {}
+
+  get(propName: string): string | number {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+}
